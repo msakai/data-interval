35017d67c7b804d8b2905f64a576be34c80c4862
Bodigrim <andrew.lelechenko@gmail.com>
2019-03-04 18:36:14 +0000

    Revert automatic deriving of Data and Generic

--- src/Data/Interval/Internal.hs
+++ src/Data/Interval/Internal.hs
@@ -1,5 +1,5 @@
 {-# OPTIONS_GHC -Wall #-}
-{-# LANGUAGE CPP, DeriveDataTypeable, DeriveGeneric #-}
+{-# LANGUAGE CPP, DeriveDataTypeable #-}
 {-# LANGUAGE Safe #-}
 #if __GLASGOW_HASKELL__ >= 708
 {-# LANGUAGE RoleAnnotations #-}
@@ -17,7 +17,6 @@ import Control.DeepSeq
 import Data.Data
 import Data.ExtendedReal
 import Data.Hashable
-import GHC.Generics (Generic)
 
 -- | The intervals (/i.e./ connected and convex subsets) over real numbers __R__.
 data Interval r = Interval
@@ -27,15 +26,32 @@ data Interval r = Interval
   , -- | 'upperBound' of the interval and whether it is included in the interval.
     -- The result is convenient to use as an argument for 'interval'.
     upperBound' :: !(Extended r, Bool)
-  } deriving (Eq, Generic, Data, Typeable)
+  } deriving (Eq, Typeable)
 
 #if __GLASGOW_HASKELL__ >= 708
 type role Interval nominal
 #endif
 
-instance NFData r => NFData (Interval r)
+instance (Ord r, Data r) => Data (Interval r) where
+  gfoldl k z x   = z interval `k` lowerBound' x `k` upperBound' x
+  toConstr _     = intervalConstr
+  gunfold k z c  = case constrIndex c of
+    1 -> k (k (z interval))
+    _ -> error "gunfold"
+  dataTypeOf _   = intervalDataType
+  dataCast1 f    = gcast1 f
 
-instance Hashable r => Hashable (Interval r)
+intervalConstr :: Constr
+intervalConstr = mkConstr intervalDataType "interval" [] Prefix
+
+intervalDataType :: DataType
+intervalDataType = mkDataType "Data.Interval.Internal.Interval" [intervalConstr]
+
+instance NFData r => NFData (Interval r) where
+  rnf (Interval lb ub) = rnf lb `seq` rnf ub
+
+instance Hashable r => Hashable (Interval r) where
+  hashWithSalt s (Interval lb ub) = s `hashWithSalt` lb `hashWithSalt` ub
 
 -- | empty (contradicting) interval
 empty :: Ord r => Interval r
