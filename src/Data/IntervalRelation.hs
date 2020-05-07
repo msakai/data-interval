12d2f8901d1fa0b60ffe8fcccbf6c3ce2b96b0fe
Marco Perone <pasafama@gmail.com>
2020-05-07 18:11:27 +0200

    complete definition of `relate`

--- src/Data/IntervalRelation.hs
+++ src/Data/IntervalRelation.hs
@@ -16,9 +16,11 @@
 -----------------------------------------------------------------------------
 module Data.IntervalRelation
   ( Relation
+  , relate
   )
   where
 
+import Prelude hiding (null)
 import Data.Data
 import Data.Interval
 import GHC.Generics (Generic)
@@ -41,18 +43,28 @@ data Relation
   | OverlappedBy
   deriving (Eq, Ord, Enum, Bounded, Show, Read, Generic, Data, Typeable)
 
+-- | Computes how two intervals are related according to the @`Relation`@ classification
 relate :: Ord r => Interval r -> Interval r -> Relation
-relate interval1 interval2 =
-  case (interval1 `intersection` interval2 == interval1, interval1 `intersection` interval2 == interval2) of
+relate i1 i2 =
+  case (i1 `intersection` i2 == i1, i1 `intersection` i2 == i2) of
     -- both intervals are equal to their intersection, hence they are equal
     (True , True ) -> Equal
-    -- 'interval1' is equal to the intersection, hence it must be strictly contained in `interval2`
-    (True , False) | lowerBound interval1 == lowerBound interval2 -> Starts
-                   | upperBound interval1 == upperBound interval2 -> Finishes
+    -- 'i1' is equal to the intersection, hence it must be strictly contained in `i2`
+    (True , False) | lowerBound i1 == lowerBound i2 -> Starts
+                   | upperBound i1 == upperBound i2 -> Finishes
                    | otherwise                                    -> During
-    -- 'interval2' is equal to the intersection, hence it must be strictly contained in `interval1`
-    (False, True ) | lowerBound interval1 == lowerBound interval2 -> StartedBy
-                   | upperBound interval1 == upperBound interval2 -> FinishedBy
+    -- 'i2' is equal to the intersection, hence it must be strictly contained in `i1`
+    (False, True ) | lowerBound i1 == lowerBound i2 -> StartedBy
+                   | upperBound i1 == upperBound i2 -> FinishedBy
                    | otherwise                                    -> Contains
-    -- neither `interval1` nor `interval2` is equal to the intersection, so neither is contained in the other
-    (False, False) -> _poiu
+    -- neither `i1` nor `i2` is equal to the intersection, so neither is contained in the other
+    (False, False) -> case ( null (i1 `intersection` i2)
+                           , lowerBound i1 < lowerBound i2
+                           , i1 `union` i2
+                           ) of
+      (True , True , Left  _) -> JustBefore
+      (True , True , Right _) -> Before
+      (True , False, Left  _) -> JustAfter
+      (True , False, Right _) -> After
+      (False, True , _      ) -> Overlaps
+      (False, False, _      ) -> OverlappedBy
