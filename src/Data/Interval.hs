dd9cd351725ff47bc422dad890a9502b8ba11c3b
Marco Perone <pasafama@gmail.com>
2020-06-01 11:43:02 +0200

    move `relate` to `Data.Interval`

--- src/Data/Interval.hs
+++ src/Data/Interval.hs
@@ -77,6 +77,9 @@ module Data.Interval
   -- * Operations
   , pickup
   , simplestRationalWithin
+
+  -- * Intervals relation
+  , relate
   ) where
 
 import Algebra.Lattice
@@ -84,6 +87,7 @@ import Control.Exception (assert)
 import Control.Monad hiding (join)
 import Data.ExtendedReal
 import Data.Interval.Internal
+import Data.IntervalRelation
 import Data.List hiding (null)
 import Data.Maybe
 import Data.Monoid
@@ -684,3 +688,29 @@ recipLB (x1, in1) = (recip x1, in1)
 recipUB :: (Fractional r, Ord r) => (Extended r, Boundary) -> (Extended r, Boundary)
 recipUB (0, _) = (NegInf, Open)
 recipUB (x1, in1) = (recip x1, in1)
+
+-- | Computes how two intervals are related according to the @`Relation`@ classification
+relate :: Ord r => Interval r -> Interval r -> Relation
+relate i1 i2 =
+  case (i1 `isSubsetOf` i2, i2 `isSubsetOf` i1) of
+    -- 'i1' ad 'i2' are equal
+    (True , True ) -> Equal
+    -- 'i1' is strictly contained in `i2`
+    (True , False) | lowerBound i1 == lowerBound i2 -> Starts
+                   | upperBound i1 == upperBound i2 -> Finishes
+                   | otherwise                                    -> During
+    -- 'i2' is strictly contained in `i1`
+    (False, True ) | lowerBound i1 == lowerBound i2 -> StartedBy
+                   | upperBound i1 == upperBound i2 -> FinishedBy
+                   | otherwise                                    -> Contains
+    -- neither `i1` nor `i2` is contained in the other
+    (False, False) -> case ( null (i1 `intersection` i2)
+                           , lowerBound i1 <= lowerBound i2
+                           , i1 `isConnected` i2
+                           ) of
+      (True , True , True ) -> JustBefore
+      (True , True , False) -> Before
+      (True , False, True ) -> JustAfter
+      (True , False, False) -> After
+      (False, True , _    ) -> Overlaps
+      (False, False, _    ) -> OverlappedBy
--- src/Data/IntervalRelation.hs
+++ src/Data/IntervalRelation.hs
@@ -16,14 +16,11 @@
 -----------------------------------------------------------------------------
 module Data.IntervalRelation
   ( Relation(..)
-  , relate
   , invert
   )
   where
 
-import Prelude hiding (null)
 import Data.Data
-import Data.Interval
 import GHC.Generics (Generic)
 
 -- | describes how two intervals @x@ and @y@ can be related.
@@ -43,32 +40,6 @@ data Relation
   | JustAfter
   | After
   deriving (Eq, Ord, Enum, Bounded, Show, Read, Generic, Data, Typeable)
-
--- | Computes how two intervals are related according to the @`Relation`@ classification
-relate :: Ord r => Interval r -> Interval r -> Relation
-relate i1 i2 =
-  case (i1 `isSubsetOf` i2, i2 `isSubsetOf` i1) of
-    -- 'i1' ad 'i2' are equal
-    (True , True ) -> Equal
-    -- 'i1' is strictly contained in `i2`
-    (True , False) | lowerBound i1 == lowerBound i2 -> Starts
-                   | upperBound i1 == upperBound i2 -> Finishes
-                   | otherwise                                    -> During
-    -- 'i2' is strictly contained in `i1`
-    (False, True ) | lowerBound i1 == lowerBound i2 -> StartedBy
-                   | upperBound i1 == upperBound i2 -> FinishedBy
-                   | otherwise                                    -> Contains
-    -- neither `i1` nor `i2` is contained in the other
-    (False, False) -> case ( null (i1 `intersection` i2)
-                           , lowerBound i1 <= lowerBound i2
-                           , i1 `isConnected` i2
-                           ) of
-      (True , True , True ) -> JustBefore
-      (True , True , False) -> Before
-      (True , False, True ) -> JustAfter
-      (True , False, False) -> After
-      (False, True , _    ) -> Overlaps
-      (False, False, _    ) -> OverlappedBy
 
 -- | inverts a relation, such that @'invert' ('relate' x y) = 'relate' y x@
 invert :: Relation -> Relation
